d28172fa4822bb0dfa246835e2c4b7be61bbd11c
Rayhan Fadhlan <65324331+RayhanFadhlan@users.noreply.github.com>
2024-04-24 15:26:13 +0700

    bfs multithread

--- internal/controllers/bfs.go
+++ internal/controllers/bfs.go
@@ -4,6 +4,8 @@ import (
 	"errors"
 	"fmt"
 	"math"
+	"runtime"
+	"time"
 )
 
 type Queue struct {
@@ -47,15 +49,15 @@ func (q *Queue) Peek() (string, error) {
 	}
 	return q.Elements[0], nil
 }
-func reverse(arr []string) ([]string){
+func reverse(arr []string) []string {
 	var ans []string
-	
-	for i:=len(arr)-1;i>=0;i--{
+
+	for i := len(arr) - 1; i >= 0; i-- {
 		ans = append(ans, arr[i])
 	}
 	return ans
 }
-func dfs(paths [][]string, path []string, parent map[string][]string, end string)([][]string) {
+func dfs(paths [][]string, path []string, parent map[string][]string, end string) [][]string {
 	if parent[end] == nil {
 		path = append(path, end)
 		// *path = append(*path, end)
@@ -75,14 +77,17 @@ func dfs(paths [][]string, path []string, parent map[string][]string, end string
 	return paths
 }
 
-func BFS(startURL string, targetURL string) ([][]string,[]string) {
+func BFS(startURL string, targetURL string) ([][]string, []string) {
 	fmt.Println("Solving with BFS")
 	fmt.Println("Start URL:", startURL)
 	fmt.Println("Target URL:", targetURL)
+	runtime.GOMAXPROCS(runtime.NumCPU())
 	// var adj [][]int
-	if(startURL==targetURL){
+	if startURL == targetURL {
 		return [][]string{{startURL}}, []string{startURL}
 	}
+
+	gm := NewGoRoutineManager(50)
 	maxInt := math.MaxInt32
 	adj := make(map[string][]string)
 	parent := make(map[string][]string)
@@ -105,19 +110,36 @@ func BFS(startURL string, targetURL string) ([][]string,[]string) {
 		// 	// fmt.Println(u,dist[u],"skipped")
 		// 	continue
 		// )
-		for i:=0;i<q.GetLength();i++{
-			if(dist[q.Elements[i]]<dist[targetURL]){
-			fmt.Println(q.Elements[i],dist[q.Elements[i]])
-			adj[q.Elements[i]] = getAllInternalLinks(q.Elements[i])
-			}
-		}
+		timenow4 := time.Now()
+		timenow := time.Now()
+		fmt.Println("len q", q.GetLength())
 
+		for i := 0; i < q.GetLength(); i++ {
+			i := i
+			gm.Run(func() {
+				mu.Lock()
+				check := dist[q.Elements[i]] < dist[targetURL]
+				mu.Unlock()
+				if check {
+					// fmt.Println(q.Elements[i], dist[q.Elements[i]])
+					links := getAllInternalLinks(q.Elements[i])
+					fmt.Println(q.Elements[i], "done")
+					mu.Lock()
+					adj[q.Elements[i]] = links
+					mu.Unlock()
+				}
+			})
+		}
+		wg.Wait()
+
+		fmt.Println("time elapsed1: ", time.Since(timenow))
 		// perform multithreading on this
 		length := q.GetLength()
-		for i:=0;i<length;i++{	
-			u := q.Elements[i];
-			
-			if(dist[u]>=dist[targetURL]){
+		timenow3 := time.Now()
+		for i := 0; i < length; i++ {
+			u := q.Elements[i]
+
+			if dist[u] >= dist[targetURL] {
 				continue
 			}
 			for _, v := range adj[u] {
@@ -125,13 +147,13 @@ func BFS(startURL string, targetURL string) ([][]string,[]string) {
 					dist[v] = maxInt
 				}
 			}
-			
-			var isFirst bool 
+
+			var isFirst bool
 			isFirst = false
-			
-			
+
+			// timenow2 := time.Now()
 			for i := 0; i < len(adj[u]); i++ {
-				
+
 				if dist[adj[u][i]] > dist[u]+1 {
 					dist[adj[u][i]] = dist[u] + 1
 					q.Enqueue(adj[u][i])
@@ -142,29 +164,34 @@ func BFS(startURL string, targetURL string) ([][]string,[]string) {
 					//parent[adj[u][i]].pushback
 					parent[adj[u][i]] = append(parent[adj[u][i]], u)
 				}
-				if(dist[targetURL]==1){
+				if dist[targetURL] == 1 {
 					isFirst = true
 					break
 				}
-				fmt.Println(adj[u][i],dist[adj[u][i]],"Target url: ",dist[targetURL])
+				// fmt.Println(adj[u][i], dist[adj[u][i]], "Target url: ", dist[targetURL])
 			}
-			if(isFirst){
+			// fmt.Println( "time elapsed2: ",time.Since(timenow2))
+			if isFirst {
+				// fmt.Println()
 				break
 			}
-			
+
 		}
-		q.Elements= q.Elements[length:]
+		fmt.Println("time elapsed3: ", time.Since(timenow3))
+		fmt.Println("time elapsed4 : ", time.Since(timenow4))
+		q.Elements = q.Elements[length:]
 	}
 	// fmt.Println(parent[targetURL],"debug")
 	//change bfs tree to array of array of solution
 	paths := make([][]string, 0)
 	path := make([]string, 0)
+	fmt.Println(len(parent))
 	paths = dfs(paths, path, parent, targetURL)
 	//perform multihtreading on this
-	for i:=0;i<len(paths);i++{
+	for i := 0; i < len(paths); i++ {
 		paths[i] = reverse(paths[i])
 	}
-	return paths,path
+	return paths, path
 	//fill solution type with solution
 	// Placeholder
 }
