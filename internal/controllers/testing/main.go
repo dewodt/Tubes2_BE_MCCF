1e63fcb321aab429d6c533811b90e23189569b61
mrsuiii <emeryfathanzwageri@gmail.com>
2024-04-26 20:48:35 +0700

    fix bug on bfs multiple and single path

--- internal/controllers/testing/main.go
+++ internal/controllers/testing/main.go
@@ -39,8 +39,8 @@ func IDStest() {
 func BFStest() {
 	// startURL := "https://en.wikipedia.org/wiki/Prince_Edward_Viaduct"
 	// endURL := "https://en.wikipedia.org/wiki/Humber_Cinemas"
-	startURL := "https://en.wikipedia.org/wiki/Humber_Cinemas"
-	endURL := "https://en.wikipedia.org/wiki/Prince_Edward_Viaduct"
+	// startURL := "https://en.wikipedia.org/wiki/Humber_Cinemas"
+	// endURL := "https://en.wikipedia.org/wiki/Prince_Edward_Viaduct"
 	// startURL := "https://en.wikipedia.org/wiki/United_States"
 	// endURL := "https://en.wikipedia.org/wiki/KFC"
 
@@ -49,15 +49,15 @@ func BFStest() {
 	// startURL := "https://en.wikipedia.org/wiki/United_States"
 	// endURL := "https://en.wikipedia.org/wiki/KFC"
 
-	// startURL := "https://en.wikipedia.org/wiki/Prabowo_Subianto"
-	// endURL := "https://en.wikipedia.org/wiki/Joko_Widodo"
+	startURL := "https://en.wikipedia.org/wiki/Prabowo_Subianto"
+	endURL := "https://en.wikipedia.org/wiki/Joko_Widodo"
 
 	// res := getAllInternalLinks(startURL)
 	// fmt.Println(res)
 	// results := getThumbnail(startURL)
 	startTime := time.Now()
 	// // fmt.Println(res)
-	results, traversed := controllers.BFS(startURL, endURL,false)
+	results, traversed := controllers.BFS(startURL, endURL,true)
 	elapsedTime := time.Since(startTime)
 
 	// fmt.Println(results)
